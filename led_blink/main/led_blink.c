727ac89246e96a5475b1e89817b6aedcfa7bb866
Pedro Lucas <pedroca242@gmail.com>
2024-10-02 14:14:42 -0300

    Exemplo led blink

---
+++ led_blink/main/led_blink.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED_BUILTIN 2
+#define LED_1 GPIO_NUM_32
+
+void task_LED1(void *param);
+
+void app_main(void)
+{
+    xTaskCreate(task_LED1, "LED1", 2048, NULL, 1, NULL);
+
+    gpio_reset_pin(LED_BUILTIN);
+    gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        gpio_set_level(LED_BUILTIN, 0);
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        gpio_set_level(LED_BUILTIN, 1);
+        vTaskDelay(500/portTICK_PERIOD_MS);
+    }
+}
+
+void task_LED1(void *param) {
+    gpio_reset_pin(LED_1);
+    gpio_set_direction(LED_1, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        gpio_set_level(LED_1, 0);
+        vTaskDelay(250/portTICK_PERIOD_MS);
+        gpio_set_level(LED_1, 1);
+        vTaskDelay(250/portTICK_PERIOD_MS);
+    }   
+
+    vTaskDelete(NULL);
+}
